dba0f786c94f170fcd3c80e040ed79872cddfe69
Mugdha Polimera <mugdhapolimera@saos-MacBook-Pro.local>
2024-01-31 17:20:50 -0500

    removed testing lines

--- tests/classic/test_solr_adapter.py
+++ tests/classic/test_solr_adapter.py
@@ -4,7 +4,7 @@ import unittest
 from aip.classic import solr_adapter
 from tests.stubdata import ADSRECORDS
 from adsputils import get_date
-import pdb
+
 class TestSolrAdapter(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
